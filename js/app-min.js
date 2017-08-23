9163c16291ee5f8eea0514853fb1e4ceb71aa2b3
chazmcgrill <charlietaylordev@gmail.com>
2017-08-23 19:21:21 +0100

    Added Function for Alarm Config
    function built for recording current time, updating values, preventing
    overrunning clock limits on alarm controls. click events created for
    alarm control buttons.

--- js/app-min.js
+++ js/app-min.js
@@ -1,10 +1,16 @@
 var typeFlag = true,
-		alarmSetFlag = false;
+		alarmSetFlag = false,
+    alarmData = {
+      hrs: 0,
+      mins: 0,
+      alarmSet: false
+    };
 
+// Functions
+// display clock data
 function display(hrs, mins) {
 	if(!typeFlag && hrs > 12) {
 		hrs = hrs - 12;
-		// add am pm indicator
 	}
 	if(hrs < 10) { hrs = '0' + hrs; }
 	if(mins < 10) { mins = '0' + mins; }
@@ -12,6 +18,7 @@ function display(hrs, mins) {
 	$('.mins').text(mins);
 }
 
+// get javascript time data
 function setClock() {
 	if(!alarmSetFlag){
 		var hrs = new Date().getHours(),
@@ -20,17 +27,65 @@ function setClock() {
 	}
 }
 
+// record current time
+function currentTime() {
+  alarmData.hrs = Number($('.hrs').text());
+  alarmData.mins = Number($('.mins').text());
+}
+
+// update values after clicking on controls
+function alarmControl(val, sym) {
+  sym === '+' ? alarmData[val]++ : alarmData[val]--;
+  alarmLimiter(val);
+}
+
+// dont allow above or below 24hrs or 60 secs
+function alarmLimiter(val) {
+  if (val === 'mins' && alarmData.mins > 59) {
+    alarmData.mins -= 60;
+  } else if (val === 'mins' && alarmData.mins < 0) {
+    alarmData.mins += 60;
+  } else if (val === 'hrs' && alarmData.hrs > 23) {
+    alarmData.hrs -= 24;
+  } else if (val === 'hrs' && alarmData.hrs < 0) {
+    alarmData.hrs += 24;
+  }
+  display(alarmData.hrs, alarmData.mins);
+}
+
+
+// Set interval timer that checks every second.
 setInterval(setClock, 1000);
 
+
+// Click events
 $('.type').click(function(){
 	typeFlag ? $('.type').text('24HRS') : $('.type').text('AM/PM');
 	typeFlag = !typeFlag;
 });
 
 $('.alarm').click(function(){
-  alarmSetFlag ? $('ul').css('display', 'none') :
-  $('ul').css('display', 'block');
+  if(alarmSetFlag) {
+    $('ul').css('display', 'none');
+  } else {
+    $('ul').css('display', 'block');
+    currentTime();
+  }
   alarmSetFlag = !alarmSetFlag;
+});
+
+$('.control-hrs li').each(function(){
+  $(this).click(function(){
+    sym = $(this).text();
+    alarmControl('hrs', sym);
+  });
+});
+
+$('.control-mins li').each(function(){
+  $(this).click(function(){
+    sym = $(this).text();
+    alarmControl('mins', sym);
+  });
 });
 
 
