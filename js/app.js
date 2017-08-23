a08faec802e722c07c9dcb6dcc74a9a432751191
chazmcgrill <charlietaylordev@gmail.com>
2017-08-23 15:17:53 +0100

    Added Core Files
    pug, sass and js files added with codekit precompiling

---
+++ js/app.js
@@ -0,0 +1,36 @@
+var typeFlag = true,
+		alarmSetFlag = false;
+
+function display(hrs, mins) {
+	if(!typeFlag && hrs > 12) {
+		hrs = hrs - 12;
+		// add am pm indicator
+	}
+	if(hrs < 10) { hrs = '0' + hrs; }
+	if(mins < 10) { mins = '0' + mins; }
+	$('.hrs').text(hrs);
+	$('.mins').text(mins);
+}
+
+function setClock() {
+	if(!alarmSetFlag){
+		var hrs = new Date().getHours(),
+				mins = new Date().getMinutes();
+		display(hrs, mins);
+	}
+}
+
+setInterval(setClock, 1000);
+
+$('.type').click(function(){
+	typeFlag ? $('.type').text('24HRS') : $('.type').text('AM/PM');
+	typeFlag = !typeFlag;
+});
+
+$('.alarm').click(function(){
+	alarmSetFlag = !alarmSetFlag;
+	// show adjusment controls
+	// blink display
+	//
+
+});
